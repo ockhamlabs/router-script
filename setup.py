e7fd0946a4ecf6da70def0720d1e8f730ba263f4
sadathknorket <sadath@knorket.ai>
2024-07-16 22:56:36 +0530

    rename dir

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Knorket_TrustMesh',
-    version='0.1.0',
+    version='0.2.0',
     author='ockhamlabs',
     author_email='hello@ockhamlabs.ai',
     description='Knorket router exposes internal private services via zero-trust',
